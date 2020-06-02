c10bde411b8b13a80729cb725a5b25cff14f22af
Raj Verma <rjnshvrm23@gmail.com>
2020-06-03 00:39:00 +0530

    Initial Commit

---
+++ SalaryCalculator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SalaryCalculator
+//
+//  Created by Raj Verma on 03/06/20.
+//  Copyright © 2020 rajverma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
